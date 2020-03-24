e57fd4fd720e589e2eb21a839aabb0b0e1507caf
董金叶 <spiroo@126.com>
2020-03-24 16:43:57 +0800

    init

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
-};
+  presets: [
+    ['@vue/cli-plugin-babel/preset', {
+      useBuiltIns: 'entry'
+    }],
+    '@babel/preset-env'
+  ],
+  plugins: [
+    [
+      'import',
+      {
+        libraryName: 'vant',
+        libraryDirectory: 'es',
+        // 指定样式路径
+        style: name => `${name}/style/less`
+      },
+      'vant'
+    ]
+  ]
+}
--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// 引入全局样式
+import '@/index.less'
+
+// 全局引入按需引入UI库 vant
+import '@/plugins/vant'
+
+// 移动端适配
+import 'lib-flexible/flexible.js'
+
+// filters
+import './filters'
+
 Vue.config.productionTip = false;
 
 new Vue({

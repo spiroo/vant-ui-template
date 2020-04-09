79d03d1bc5db93d62caee11fecd5192186614555
spiroo <spiroo@126.com>
2020-04-09 12:06:24 +0800

    add svg and gzip

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 import store from '@/store';
 import { Toast } from 'vant';
-// 根据环境不同引入不同api地址
-import { baseApi } from '@/config';
 
 // create an axios instance
 const service = axios.create({
-  baseURL: baseApi, // url = base api url + request url
+  baseURL: '/', // url = base api url + request url
   withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 });
